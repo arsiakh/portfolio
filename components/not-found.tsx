54958baafb7e9a37533aad12e03af7c425e5290c
Arsia Khorramijam <a.khorramijam@gmail.com>
2024-12-29 21:21:33 -0500

    fixed minor apostraphe issue

--- components/not-found.tsx
+++ components/not-found.tsx
@@ -4,7 +4,7 @@ export default function ProjectNotFound() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen px-4 text-center">
       <h1 className="text-4xl font-bold text-gray-200 mb-4">Project Not Found</h1>
-      <p className="text-gray-400 mb-8">Sorry, the project you're looking for doesn't exist.</p>
+      <p className="text-gray-400 mb-8">Sorry, the project you&apos;re looking for doesn&apos;t exist.</p>
       <Link href="/" className="text-white hover:text-white/90 underline">
         Return to Home
       </Link>
